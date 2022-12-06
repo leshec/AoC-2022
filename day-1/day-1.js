c75defecb70789ae9c76ee24f50e71a0cea88804
leshec <richardmcewan100@gmail.com>
2022-12-06 21:12:24 +0000

    rm unwanted comments

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -3,7 +3,6 @@ let biggest = new Array();
 
 const {readFileSync, promises: fsPromises} = require('fs');
 
-// read file SYNCHRONOUSLY
 function syncReadFile(filename) {
     const contents = readFileSync(filename, 'utf-8');
 
@@ -18,13 +17,9 @@ function sum(data){
     let sum = 0;
     for(let i=0; i < data.length; i++){
 
-        //NASTY BUG HERE AT END OF ARRAY SHOULD BE OUT OF BOUNDS BUT FINDING EMPY
-        //SURELY LENGTH -1
-
         if(data[i]==""){
              biggest.push(sum);
             sum = 0;
-
         }
         else {
             value = parseInt(data[i]); 
@@ -35,9 +30,4 @@ function sum(data){
     return console.log(biggest[0]+biggest[1]+biggest[2]);
 }
 
-/*
-function updateBiggest(sum){
-    biggest.push(sum);
-}
-*/
 
